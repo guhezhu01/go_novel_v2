e8038c2a6a0c4a87ff6516fa9adb11fece34aca7
guhezhu01 <12369945+guhezhu01@user.noreply.gitee.com>
2023-08-15 23:44:17 +0800

    init

--- api-gateway/routes/routes.go
+++ api-gateway/routes/routes.go
@@ -31,7 +31,6 @@ func InitRoutes() *gin.Engine {
 
 	engine := gin.Default()
 	middleware.Limit(engine)
-	gin.SetMode(gin.ReleaseMode)
 	engine.Use(middleware.Cors(), middleware.Logger(), timeoutMiddleware())
 	auth := engine.Group("api/v1")
 	auth.Use(middleware.JwtToken())
