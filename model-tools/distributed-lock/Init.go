5fc6e0097fb58c441fd54fcad700d3f8d6def737
guhezhu01 <2022246944@qq.com>
2023-08-18 11:31:43 +0800

    init

--- model-tools/distributed-lock/Init.go
+++ model-tools/distributed-lock/Init.go
@@ -3,72 +3,108 @@ package distributed_lock
 import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
+	"log"
 	"time"
 )
 
 type RedisLock struct {
 	redisPool *redis.Pool
+	id        string
 	resource  string
 	expire    time.Duration
 }
 
-func InitLock(resource string) *RedisLock {
+// 对同一个数据加锁，但是只能一个进程解锁
+func InitLock(addr, password string) *RedisLock {
 	pool := &redis.Pool{
 		MaxIdle:     30,          // 最大连接数
 		MaxActive:   100,         //最大活跃连接数，0代表无限
 		IdleTimeout: time.Minute, //闲置连接的超时时间
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "localhost:6379")
+
+			c, err := redis.Dial("tcp", addr)
+			if err != nil {
+				return nil, err
+			}
+			if password != "" {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+
 		},
 	}
-	lock := NewRedisLock(pool, resource, 10*time.Second)
+	lock := newRedisLock(pool, 10*time.Second)
 	return lock
 }
 
-func NewRedisLock(pool *redis.Pool, resource string, expire time.Duration) *RedisLock {
+func newRedisLock(pool *redis.Pool, expire time.Duration) *RedisLock {
 
 	return &RedisLock{
 		redisPool: pool,
-		resource:  resource,
 		expire:    expire,
 	}
 
 }
 
-func (lock *RedisLock) TryLock() bool {
+// TryLock 加锁 argsId用于设置进程唯一id,防止其他进程解锁该进程操作的resource(默认不指定)
+func (lock *RedisLock) TryLock(resource string, timeOut time.Duration, argsId ...string) bool {
+
+	var id string
+	if len(argsId) > 0 {
+		id = argsId[0]
+	}
+	lock.id = id
+	lock.resource = resource
 
 	conn := lock.redisPool.Get()
-
 	defer conn.Close()
-
 	// 尝试获取锁
-
-	result, err := redis.String(conn.Do("SET", lock.resource, "1", "EX", int(lock.expire.Seconds()), "NX"))
-
-	if err != nil {
-
-		fmt.Println("尝试获取锁发生错误：", err)
-
-		return false
-
+	now := time.Now()
+	for {
+		// set lock x EX 10 NX (EX设置过期时间,NX:只在键不存在的时候，才对键进行设置操作)
+		// SET key value NX 等同于 SETNX key value
+		result, err := redis.String(conn.Do("SET", resource, "1", "EX", int(lock.expire.Seconds()), "NX"))
+		if err == nil && result == "OK" {
+			log.Println("加锁成功!")
+			return true
+		}
+		since := time.Since(now)
+		// 判断是否超时
+		if since >= timeOut {
+			log.Println("尝试获取锁超时: ", err)
+			return false
+		}
 	}
 
-	return result == "OK"
+	return true
 
 }
 
-func (lock *RedisLock) Unlock() bool {
+// Unlock 释放锁
+func (lock *RedisLock) Unlock(argsId ...string) bool {
+	if lock.id != "" {
+		if len(argsId) > 0 {
+			if argsId[0] != lock.id {
+				fmt.Println("没有解锁权限, id = ", argsId[0])
+				return false
+			}
+		} else {
+			fmt.Println("解锁失败, id = ", argsId[0])
+			return false
+		}
+	}
 
 	conn := lock.redisPool.Get()
-
 	defer conn.Close()
-
 	_, err := conn.Do("DEL", lock.resource)
 
 	if err != nil {
 		fmt.Println("释放锁发生错误：", err)
 		return false
 	}
-
+	fmt.Println("释放锁成功!")
 	return true
 }
---
+++ model-tools/distributed-lock/Init_test.go
@@ -0,0 +1,21 @@
+package distributed_lock
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestA(t *testing.T) {
+	lock := InitLock("123.249.88.132:6379", "3077267500zJ.")
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			lock.TryLock("aod", time.Millisecond*1000)
+			lock.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
